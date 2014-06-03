48b40d4cd229c3f89bdfb939a66c8add1d2db2eb
Dominik Richter <dominik.richter@gmail.com>
2014-06-03 17:02:27 +0200

    feature: added conditional cipher checks
    implemented for ubuntu right now
    
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- default/serverspec/ssh_spec.rb
+++ default/serverspec/ssh_spec.rb
@@ -71,7 +71,24 @@ describe 'check sshd_config' do
   end
 
   describe file('/etc/ssh/sshd_config') do
-    its(:content) { should match(/^Ciphers (aes128-ctr,aes256-ctr,aes192-ctr)|(aes128-ctr,aes256-ctr,aes192-ctr,aes128-cbc,aes256-cbc,aes192-cbc)$/) }
+    its(:content) do
+
+      # define a set of default ciphers
+      ciphers = 'aes128-ctr,aes256-ctr,aes192-ctr'
+
+      # adjust ciphers based on OS + release
+      case os[:family]
+      when 'Ubuntu'
+        case os[:release]
+        when '12.04'
+          ciphers = 'aes128-ctr,aes192-ctr,aes256-ctr'
+        when '14.04'
+          ciphers = 'chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes128-ctr,aes192-ctr,aes256-ctr'
+        end
+      end
+
+      should match(/^Ciphers #{ciphers}$/)
+    end
   end
 
   describe file('/etc/ssh/sshd_config') do
