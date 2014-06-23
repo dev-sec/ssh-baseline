b6e6187af75d5ccfd110db89fb702b2ee78a0341
Dominik Richter <dominik.richter@gmail.com>
2014-06-23 15:46:03 +0200

    bugfix: use simple ruby tests for custom rspec matcher
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- default/serverspec/ssh_spec.rb
+++ default/serverspec/ssh_spec.rb
@@ -35,7 +35,7 @@ RSpec::Matchers.define :valid_cipher do
       end
     end
 
-    expect(actual).to match(/^Ciphers #{ciphers}$/)
+    actual =~ /^Ciphers #{ciphers}$/
   end
 end
 
@@ -71,9 +71,9 @@ RSpec::Matchers.define :valid_kex do
     end
 
     if kex.nil?
-      expect(actual).to_not match(/^KexAlgorithms/)
+      actual !~ /^KexAlgorithms/
     else
-      expect(actual).to match(/^KexAlgorithms #{kex}$/)
+      actual =~ /^KexAlgorithms #{kex}$/
     end
   end
 end
@@ -110,7 +110,7 @@ RSpec::Matchers.define :valid_mac do
       end
     end
 
-    expect(actual).to match(/^MACs #{macs}$/)
+    actual =~ /^MACs #{macs}$/
   end
 end
 
