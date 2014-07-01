b4f4c150948f9f626e1e6acdd0c60fa479d14211
Dominik Richter <dominik.richter@gmail.com>
2014-07-01 12:17:14 +0200

    bugfix: unlock user accounts on test systems
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- default/puppet/manifests/site.pp
+++ default/puppet/manifests/site.pp
@@ -1,1 +1,12 @@
+# workaround for unlocking user accounts:
+# Locked user accounts are identified via '!' in /etc/shadow
+# SSH will deny login to locked accounts, unless UsePAM is active
+# To keep UsePAM dactivated, user accounts are 'unlocked',
+# but still get an impossible password - so the aim of locking
+# is still present, while SSH login is possible.
+exec { "unlock users":
+  command => "sed 's/^\\([^:]*:\\)\\!/\\1*/' -i /etc/shadow",
+  onlyif => 'test -n "$(grep "^[^:]*:\!" -o /etc/shadow)"'
+}
+
 class { 'ssh_hardening': }
