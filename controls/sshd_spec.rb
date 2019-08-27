6a2c90fd657bbe56a96d59be6d8c2478f876175d
Christopher <christopher.biel89@gmail.com>
2019-08-27 16:13:21 +0200

    Issue 139 - Use cmp instead of be matcher to allow string matching (dev-sec#139) (#140)
    Signed-off-by: Christopher Biel <christopher.biel89@gmail.com>

--- controls/sshd_spec.rb
+++ controls/sshd_spec.rb
@@ -222,7 +222,7 @@ control 'sshd-19' do
   tag 'CIS Red Hat Enterprise Linux 7 Benchmark version 01-31-2017': '2.1.1'
   ref 'Center for Internet Security', url: 'https://www.cisecurity.org/'
   describe sshd_config do
-    its('MaxAuthTries') { should be == attribute('max_auth_tries') }
+    its('MaxAuthTries') { should cmp == attribute('max_auth_tries') }
   end
 end
 
