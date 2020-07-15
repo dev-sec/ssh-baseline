30e98c10b64418aa62774a41733790aa5ff1602a
Martin Schurz <Martin.Schurz@t-systems.com>
2020-07-15 18:47:28 +0200

    add control for CRYPTO_POLICY on RedHat
    RedHat introduces a CRYPTO_POLICY in RHEL8. This needs to be configured
    separately, or it will override sshd_config settings for Cipher, MAC and
    Kex.
    
    see: https://access.redhat.com/solutions/4410591
    
    Signed-off-by: Martin Schurz <Martin.Schurz@t-systems.com>

--- controls/sshd_spec.rb
+++ controls/sshd_spec.rb
@@ -503,3 +503,17 @@ control 'sshd-48' do
     its('stderr') { should eq '' }
   end
 end
+
+control 'sshd-49' do
+  impact 1.0
+  title 'Server: CRYPTO_POLICY'
+  desc 'Verifies, that we are not running CRYPTO_POLICY and our settings from sshd_config are effective'
+  if os[:family] == "redhat" && ::Gem::Version.new(os.release) > ::Gem::Version.new('8')
+    describe bash("pgrep -af 'sshd -D'") do
+      its('exit_status') { should eq 0 }
+      its('stdout') { should_not match('-oCiphers') }
+      its('stdout') { should_not match('-oKexAlgorithms') }
+      its('stdout') { should_not match('-oHostKeyAlgorithms') }
+    end
+  end
+end
